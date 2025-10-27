e86589b65c1f8f3e90bfdd3d6936257b74e4aff9
js82ksd <58346736+js82ksd@users.noreply.github.com>
2025-10-27 17:20:46 +0100

    Update ContinuitySpam.java

--- app/src/main/java/com/tutozz/blespam/ContinuitySpam.java
+++ app/src/main/java/com/tutozz/blespam/ContinuitySpam.java
@@ -1,73 +1,26 @@
-package com.tutozz.blespam;
-
-import android.bluetooth.le.AdvertiseData;
-import android.bluetooth.le.AdvertiseSettings;
-import android.util.Log;
-
-public class ContinuitySpam extends Spammer {
-    private ContinuityDevice.type deviceType;
-    private boolean isCrash;
-    
-    public ContinuitySpam(ContinuityDevice.type deviceType, boolean isCrash) {
-        this.deviceType = deviceType;
-        this.isCrash = isCrash;
-    }
-    
-    @Override
-    public void start() {
-        isSpamming = true;
+case DEVICE:
+    devices = new ContinuityDevice[]{
+        // ОРИГИНАЛЬНЫЕ УСТРОЙСТВА
+        new ContinuityDevice("0x0E20", "AirPods Pro", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0620", "Beats Solo 3", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0A20", "AirPods Max", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x1020", "Beats Flex", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0055", "Airtag", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0030", "Hermes Airtag", ContinuityDevice.type.DEVICE),
         
-        try {
-            // 🎯 ИСПОЛЬЗУЕМ ШУТОЧНЫЕ УСТРОЙСТВА ВМЕСТО ФИКСИРОВАННЫХ
-            ContinuityDevice jokeDevice = ContinuityDevice.getRandomJokeDevice();
-            String deviceName = jokeDevice.getName();
-            String hexData = jokeDevice.getValue();
-            
-            Log.d("CONTINUITY_SPAM", "🚀 Starting: " + deviceName);
-            
-            // Конвертируем hex в byte array
-            byte[] manufacturerData = hexStringToByteArray(hexData);
-            
-            AdvertiseData advertiseData = new AdvertiseData.Builder()
-                .setIncludeDeviceName(true)
-                .setIncludeTxPowerLevel(true)
-                .addManufacturerData(0x004C, manufacturerData)
-                .build();
-                
-            AdvertiseSettings advertiseSettings = new AdvertiseSettings.Builder()
-                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
-                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
-                .setConnectable(false)
-                .setTimeout(0)
-                .build();
-                
-            // Используем оригинальный метод из Spammer класса
-            startAdvertising(advertiseSettings, advertiseData);
-            
-        } catch (Exception e) {
-            Log.e("CONTINUITY_SPAM", "Error: " + e.getMessage());
-        }
-    }
-    
-    @Override
-    public void stop() {
-        isSpamming = false;
-        stopAdvertising();
-    }
-    
-    // Вспомогательный метод для конвертации hex строки в byte array
-    private byte[] hexStringToByteArray(String s) {
-        try {
-            int len = s.length();
-            byte[] data = new byte[len / 2];
-            for (int i = 0; i < len; i += 2) {
-                data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                                     + Character.digit(s.charAt(i + 1), 16));
-            }
-            return data;
-        } catch (Exception e) {
-            Log.e("CONTINUITY_SPAM", "Error converting hex: " + e.getMessage());
-            return new byte[0];
-        }
-    }
-}
+        // 🎯 ШУТОЧНЫЕ УСТРОЙСТВА - ДОБАВЛЯЕМ СЮДА
+        new ContinuityDevice("0x0055", "🚨 POLICE iPhone", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0030", "📡 GOV iPad", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0E20", "👽 UFO Device", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0620", "💣 Bomb Squad", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0A20", "🚗 Tesla Security", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x1020", "🛰️ NSA Monitor", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0220", "⚡ Energy Dept", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0F20", "🎯 Military Grade", ContinuityDevice.type.DEVICE),
+        
+        // ОСТАЛЬНЫЕ ОРИГИНАЛЬНЫЕ
+        new ContinuityDevice("0x0220", "AirPods", ContinuityDevice.type.DEVICE),
+        new ContinuityDevice("0x0F20", "AirPods 2nd Gen", ContinuityDevice.type.DEVICE),
+        // ... остальные оригинальные устройства
+    };
+    break;
